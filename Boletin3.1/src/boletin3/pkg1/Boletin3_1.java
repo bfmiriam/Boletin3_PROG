dc36884c7b920b2d9418ad70b91c1352cd92589f
mbacelofernandez <mbacelofernandez@victoria12.danielcastelao.org>
2017-11-20 13:15:38 +0100

    Inicio proyecto

---
+++ Boletin3.1/src/boletin3/pkg1/Boletin3_1.java
@@ -0,0 +1,27 @@
+
+package boletin3.pkg1;
+
+
+
+
+public class Boletin3_1 {
+
+   
+    public static void main(String[] args) {
+     
+        
+        
+   Coche coche1 = new Coche ();
+   
+
+   coche1.acelerar(10);
+   System.out.println("Incrementa "+10+" km/h");
+   
+   coche1.frenar(29);
+    System.out.println("Diminue "+29+" km/h");
+    
+
+   
+    }
+    
+}
