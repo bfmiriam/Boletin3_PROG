0c92fbb2935f96f3fbcbf3c7d28741ab11996427
mbacelofernandez <mbacelofernandez@victoria12.danielcastelao.org>
2017-11-20 13:16:26 +0100

    Inicio proyecto

---
+++ Boletin4_2/src/boletin4_2/Boletin4_2.java
@@ -0,0 +1,18 @@
+
+package boletin4_2;
+
+
+public class Boletin4_2 {
+
+  
+    public static void main(String[] args) {
+  
+     Satelite satelite1 = new Satelite ();
+     
+     
+     satelite1.verPosicion(2, 3, 4);
+        
+        
+    }
+    
+}
---
+++ Boletin4_2/src/boletin4_2/Satelite.java
@@ -0,0 +1,36 @@
+
+package boletin4_2;
+
+
+public class Satelite {
+    
+     private double meridiano ;
+     private double paralelo;
+     private double distanciaTerra ;
+  
+
+public Satelite(){
+    
+ meridiano= 0;
+ paralelo= 0;
+ distanciaTerra=0 ;  
+    
+
+}
+
+
+
+public Satelite (double m, double p, double d) {
+
+ meridiano= m;
+ paralelo= p;
+ distanciaTerra=d ;
+
+}
+
+public void verPosicion (double paralelo, double meridiano, double distanciaTerra) {
+
+System.out.println(" o satelite atopase  no paralelo "+  paralelo + " meridiano " + meridiano + " a unha distancia da terra " + distanciaTerra );
+
+}
+}
