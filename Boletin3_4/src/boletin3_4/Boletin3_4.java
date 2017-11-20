c27fe076ba8efcf5fc33af0d42c01025b31219ac
mbacelofernandez <mbacelofernandez@victoria12.danielcastelao.org>
2017-11-20 13:16:05 +0100

    Inicio proyecto

---
+++ Boletin3_4/src/boletin3_4/Boletin3_4.java
@@ -0,0 +1,12 @@
+
+package boletin3_4;
+
+
+public class Boletin3_4 {
+
+  
+    public static void main(String[] args) {
+        
+    }
+    
+}
---
+++ Boletin3_4/src/boletin3_4/Restaurante.java
@@ -0,0 +1,15 @@
+
+package boletin3_4;
+
+
+public class Restaurante {
+    
+  private int cantPatacas;
+  private int cantPolbo;
+  private int numClientes;
+  
+  
+public int calcularClientes (int cantPatacas, int cantPolbo);
+    
+
+}
